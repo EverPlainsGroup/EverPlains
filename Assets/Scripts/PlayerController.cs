c769612ee1263eda20790e971eb54bf663a5c78e
rivera26 <rivera26@pdx.edu>
2021-07-16 16:28:03 -0700

    movement cleanup
    - added fall animation
    - added Jump()
    - changed isGrounded from a bool to its own function called IsGrounded
    
    - added cinemachine from Unit asset store

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,58 +4,121 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
-    public float jumpForce;
-    private float moveInput;
+    public float speed = 2;
+    public float jumpForce = 4;
+    private bool isFacingRight = true;
 
     // Variables for checking if player is grounded
-    private bool isGrounded;
-    public Transform groundCheck;
+    private bool grounded = false;
+    public Transform feet;
     public float checkRadius;
     public LayerMask ground;
 
     // Jump count
-    private int jumpCount;
-    public int jumpCountValue;
+    private int extraJumpCount;
+    public int extraJumpCountValue = 1;
 
-    private Rigidbody2D rb;
+    public Rigidbody2D rb;
+    public SpriteRenderer sr;
+    public PolygonCollider2D pc;
+    public Animator animator;
 
-    // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
-        jumpCount = jumpCountValue;
+        sr = GetComponent<SpriteRenderer>();
+        pc = GetComponent<PolygonCollider2D>();
+        extraJumpCount = extraJumpCountValue;
     }
 
     void FixedUpdate()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, ground);
-
         if (Input.GetKey(KeyCode.A))
         {
-            GetComponent<SpriteRenderer>().flipX = true;
+            if (isFacingRight) FlipPlayer();
             rb.velocity = new Vector2(-1 * speed, rb.velocity.y);
+            animator.SetInteger("AnimState", 1);
         }
 
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
-            GetComponent<SpriteRenderer>().flipX = false;
+            if (!isFacingRight) FlipPlayer();
             rb.velocity = new Vector2(speed, rb.velocity.y);
+            animator.SetInteger("AnimState", 1);
+        }
+
+        // Idle
+        else
+        {
+            animator.SetInteger("AnimState", 0);
         }
     }
     void Update()
     {
-        if (isGrounded == true)
+        // Set animator AirSpeedY equal to vertical velocity
+        animator.SetFloat("AirSpeedY", rb.velocity.y);
+
+        // Check if character just landed on the ground
+        if (!grounded && IsGrounded())
         {
-            jumpCount = jumpCountValue;
+            grounded = true;
+            animator.SetBool("Grounded", grounded);
+            extraJumpCount = extraJumpCountValue;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && jumpCount > 0)
+        // Check if character just started falling
+        if (grounded && !IsGrounded())
         {
-            rb.velocity = Vector2.up * jumpForce;
-            --jumpCount;
+            grounded = false;
+            animator.SetBool("Grounded", grounded);
         }
-        else if (Input.GetKeyDown(KeyCode.Space) && jumpCount == 0 && isGrounded == true)
+
+        // Jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    // Uses a sensor to detect if the player's "feet"
+    // are touching the ground
+    public bool IsGrounded()
+    {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, checkRadius, ground);
+
+        if (groundCheck)
+        {
+            return true;
+        }
+
+        else
+        {
+            return false;
+        }
+    }
+
+    // Flips the Player object, including all children
+    void FlipPlayer()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+
+    void Jump()
+    {
+        if (extraJumpCount > 0)
+        {
+            animator.SetTrigger("Jump");
+            grounded = false;
+            animator.SetBool("Grounded", IsGrounded());
+            rb.velocity = Vector2.up * jumpForce;
+            --extraJumpCount;
+        }
+
+        else if (extraJumpCount == 0 && IsGrounded() == true)
         {
             rb.velocity = Vector2.up * jumpForce;
         }
