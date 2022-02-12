76b4f50435fea7e2ffaeb9d67cc58d7450fe662e
Tram Vuong <vuong2@pdx.edu>
2022-02-11 23:32:53 -0800

    first pass refactoring enemies

--- Assets/Scripts/BatController.cs
+++ Assets/Scripts/BatController.cs
@@ -3,63 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class BatController : MonoBehaviour {
-  public Rigidbody2D rb;
-  public SpriteRenderer sr;
-  public PolygonCollider2D pc;
-  public Animator animator;
-  public Transform target;
-  public float range = 5;
-  public float speed = 1;
-  public int maxHP = 50;
-  private int currentHP;
-  private bool isFacingRight = false;
-  public GameObject attackPoint;
-  public float attackRange = 2;
-  public float attackTimer;
-  public Rigidbody2D goldCoin;
-  public float distance;
-
+public class BatController : EnemyController {
+  public BatController() : base() {
+      attackRange = 2;
+  } 
   // Start is called before the first frame update
-  void Start() {
-    target = GameObject.FindWithTag("Player").transform;
-    animator = GetComponent<Animator>();
-    rb = GetComponent<Rigidbody2D>();
-    sr = GetComponent<SpriteRenderer>();
-    pc = GetComponent<PolygonCollider2D>();
-    currentHP = maxHP;
-    attackPoint.SetActive(false);
-  }
+//   void Start() {
+//     target = GameObject.FindWithTag("Player").transform;
+//     animator = GetComponent<Animator>();
+//     rb = GetComponent<Rigidbody2D>();
+//     sr = GetComponent<SpriteRenderer>();
+//     pc = GetComponent<PolygonCollider2D>();
+//     currentHP = maxHP;
+//     attackPoint.SetActive(false);
+//   }
 
   // Update is called once per frame
-  void Update() {
-    attackTimer += Time.deltaTime;
-    distance = Vector2.Distance(target.position, transform.position);
+//   void Update() {
+//     attackTimer += Time.deltaTime;
+//     distance = Vector2.Distance(target.position, transform.position);
 
-    if (distance < range) {
-      animator.SetBool("Walk", true);
+//     if (distance < range) {
+//       animator.SetBool("Walk", true);
 
-      if (isFacingRight && target.transform.position.x < transform.position.x) {
-        FlipBat();
-      } else if (!isFacingRight && target.transform.position.x > transform.position.x) {
-        FlipBat();
-      }
+//       if (GetIsFacingRight() && target.transform.position.x < transform.position.x) {
+//         Flip();
+//       } else if (!GetIsFacingRight() && target.transform.position.x > transform.position.x) {
+//         Flip();
+//       }
 
-      ChaseTarget();
-    }
+//       ChaseTarget();
+//     }
 
-    animator.SetBool("Walk", false);
+//     animator.SetBool("Walk", false);
 
+//     if (distance < attackRange) {
+//       StartCoroutine(AttackMovement());
+//     }
+
+//     if (currentHP <= 0) {
+//       Die();
+//     }
+//   }
+
+  public override void OrchestrateAttack() {
     if (distance < attackRange) {
       StartCoroutine(AttackMovement());
     }
-
-    if (currentHP <= 0) {
-      Die();
-    }
   }
-
-  IEnumerator AttackMovement() {
+  
+  public override IEnumerator AttackMovement() {
     attackPoint.SetActive(true);
     animator.SetTrigger("Attack");
     transform.position = Vector2.MoveTowards(transform.position, target.position, (speed + 0.5f) * Time.deltaTime);
@@ -73,31 +66,29 @@ public class BatController : MonoBehaviour {
     StartCoroutine(AttackMovement());
   }
 
-  void ChaseTarget() {
-    transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-  }
+//   void ChaseTarget() {
+//     transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+//   }
 
-  void Die() {
-    DropGold();
+  public override void Die() {
     rb.gravityScale = 1;
-    this.enabled = false;
-    pc.enabled = false;
+    base.Die();
   }
 
-  void DropGold() {
-    int rand = Random.Range(1, 6);
-    Vector2 randDirection;
-    int randSpeed;
+//   void DropGold() {
+//     int rand = Random.Range(1, 6);
+//     Vector2 randDirection;
+//     int randSpeed;
 
-    for (int i = 0; i < rand; ++i) {
-      randSpeed = Random.Range(100, 200);
-      randDirection = Random.insideUnitCircle.normalized;
-      Rigidbody2D coin = Instantiate(goldCoin, transform.position, transform.rotation);
-      coin.AddRelativeForce(randDirection * 200);
-    }
-  }
+//     for (int i = 0; i < rand; ++i) {
+//       randSpeed = Random.Range(100, 200);
+//       randDirection = Random.insideUnitCircle.normalized;
+//       Rigidbody2D coin = Instantiate(goldCoin, transform.position, transform.rotation);
+//       coin.AddRelativeForce(randDirection * 200);
+//     }
+//   }
 
-  IEnumerator DamageAnimation() {
+  public override IEnumerator DamageAnimation() {
     sr.color = Color.red;
     yield return new WaitForSeconds(0.1f);
     sr.color = Color.white;
@@ -105,37 +96,31 @@ public class BatController : MonoBehaviour {
     sr.color = Color.red;
     yield return new WaitForSeconds(0.4f);
 
-    if (currentHP > 0) {
+    if (!IsHPHighEnough()) {
       sr.color = Color.white;
     }
 
     rb.velocity = new Vector2(0 * speed, rb.velocity.y);
   }
 
-  void OnTriggerEnter2D(Collider2D collision) {
-    if (collision.gameObject.name == "AttackPoint") {
-      TakeDamage(25);
+//   void OnTriggerEnter2D(Collider2D collision) {
+//     if (collision.gameObject.name == "AttackPoint") {
+//       TakeDamage(25);
+//     }
+//   }
+
+//   public override void TakeDamage(int damage) {
+    // currentHP -= damage;
+
+    public override void KnockBack() {
+        // knockback
+      if (target.transform.position.x < transform.position.x) {
+        rb.velocity = new Vector2(3 * speed, rb.velocity.y);
+      } else {
+        rb.velocity = new Vector2(-3 * speed, rb.velocity.y);
+      }    
     }
-  }
 
-  void TakeDamage(int damage) {
-    currentHP -= damage;
-
-    // knockback
-    if (target.transform.position.x < transform.position.x) {
-      rb.velocity = new Vector2(3 * speed, rb.velocity.y);
-    } else {
-      rb.velocity = new Vector2(-3 * speed, rb.velocity.y);
-    }
-
-    // flash red and squish
-    StartCoroutine(DamageAnimation());
-  }
-
-  void FlipBat() {
-    isFacingRight = !isFacingRight;
-    Vector3 scaler = transform.localScale;
-    scaler.x *= -1;
-    transform.localScale = scaler;
-  }
+    // // flash red and squish
+    // StartCoroutine(DamageAnimation());
 }
--- Assets/Scripts/SlimeController.cs
+++ Assets/Scripts/SlimeController.cs
@@ -3,135 +3,85 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SlimeController : MonoBehaviour {
-  public Rigidbody2D rb;
-  public SpriteRenderer sr;
-  public PolygonCollider2D pc;
-  public Animator animator;
-  public Transform target;
-  public float range = 5;
-  public float speed = 1;
-  public int maxHP = 50;
-  private int currentHP;
-  private bool isFacingRight = false;
-  public GameObject attackPoint;
-  public float attackRange = 0.5f;
-  public float attackTimer;
-  public Rigidbody2D goldCoin;
-  public float distance;
-
+public class SlimeController : EnemyController {
+  public SlimeController() : base() {
+      attackRange = 0.5f;
+  } 
   // Start is called before the first frame update
-  void Start() {
-    target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-    animator = GetComponent<Animator>();
-    rb = GetComponent<Rigidbody2D>();
-    sr = GetComponent<SpriteRenderer>();
-    pc = GetComponent<PolygonCollider2D>();
-    currentHP = maxHP;
-    attackPoint.SetActive(false);
-  }
+//   void Start() {
+//     target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+//     animator = GetComponent<Animator>();
+//     rb = GetComponent<Rigidbody2D>();
+//     sr = GetComponent<SpriteRenderer>();
+//     pc = GetComponent<PolygonCollider2D>();
+//     currentHP = maxHP;
+//     attackPoint.SetActive(false);
+//   }
 
-  // Update is called once per frame
-  void Update() {
-    attackTimer += Time.deltaTime;
-    distance = Vector2.Distance(target.position, transform.position);
+//   public override IEnumerator AttackMovement() {
+//     animator.SetTrigger("Attack");
+//     yield return new WaitForSeconds(0.7f);
+//     attackPoint.SetActive(true);
+//     yield return new WaitForSeconds(0.3f);
+//     attackPoint.SetActive(false);
+//   }
 
-    if (distance < range) {
-      animator.SetBool("Walk", true);
+//   void ChaseTarget() {
+//     transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+//   }
 
-      if (isFacingRight && target.position.x < transform.position.x) {
-        FlipSlime();
-      } else if (!isFacingRight && target.position.x > transform.position.x) {
-        FlipSlime();
-      }
+//   void Die() {
+//     DropGold();
+//     this.enabled = false;
+//     pc.enabled = false;
+//   }
 
-      ChaseTarget();
-    }
+//   void DropGold() {
+//     int rand = Random.Range(1, 6);
+//     Vector2 randDirection;
+//     int randSpeed;
 
-    animator.SetBool("Walk", false);
+//     for (int i = 0; i < rand; ++i) {
+//       randSpeed = Random.Range(100, 200);
+//       randDirection = Random.insideUnitCircle.normalized;
+//       Rigidbody2D coin = Instantiate(goldCoin, transform.position, transform.rotation);
+//       coin.AddRelativeForce(randDirection * 200);
+//     }
+//   }
 
-    if (distance < attackRange && attackTimer > 1.25) {
-      StartCoroutine(AttackCollision());
-      attackTimer = 0.0f;
-    }
+//   IEnumerator DamageAnimation() {
+//     var originalScale = transform.localScale;
+//     transform.localScale = new Vector2(0.5f * originalScale.x, originalScale.y);
+//     sr.color = Color.red;
+//     yield return new WaitForSeconds(0.1f);
+//     sr.color = Color.white;
+//     yield return new WaitForSeconds(0.1f);
+//     sr.color = Color.red;
+//     yield return new WaitForSeconds(0.4f);
 
-    if (currentHP <= 0) {
-      Die();
-    }
+//     if (currentHP > 0) {
+//       sr.color = Color.white;
+//       transform.localScale = originalScale;
+//     }
+//   }
 
-  }
+//   void OnTriggerEnter2D(Collider2D collision) {
+//     if (collision.gameObject.name == "AttackPoint") {
+//       TakeDamage(25);
+//     }
+//   }
 
-  IEnumerator AttackCollision() {
-    animator.SetTrigger("Attack");
-    yield return new WaitForSeconds(0.7f);
-    attackPoint.SetActive(true);
-    yield return new WaitForSeconds(0.3f);
-    attackPoint.SetActive(false);
-  }
+//   void TakeDamage(int damage) {
+//     currentHP -= damage;
 
-  void ChaseTarget() {
-    transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-  }
+//     // knockback
+//     if (target.position.x < transform.position.x) {
+//       rb.velocity = new Vector2(5 * speed, rb.velocity.y);
+//     } else {
+//       rb.velocity = new Vector2(-5 * speed, rb.velocity.y);
+//     }
 
-  void Die() {
-    DropGold();
-    this.enabled = false;
-    pc.enabled = false;
-  }
-
-  void DropGold() {
-    int rand = Random.Range(1, 6);
-    Vector2 randDirection;
-    int randSpeed;
-
-    for (int i = 0; i < rand; ++i) {
-      randSpeed = Random.Range(100, 200);
-      randDirection = Random.insideUnitCircle.normalized;
-      Rigidbody2D coin = Instantiate(goldCoin, transform.position, transform.rotation);
-      coin.AddRelativeForce(randDirection * 200);
-    }
-  }
-
-  IEnumerator DamageAnimation() {
-    var originalScale = transform.localScale;
-    transform.localScale = new Vector2(0.5f * originalScale.x, originalScale.y);
-    sr.color = Color.red;
-    yield return new WaitForSeconds(0.1f);
-    sr.color = Color.white;
-    yield return new WaitForSeconds(0.1f);
-    sr.color = Color.red;
-    yield return new WaitForSeconds(0.4f);
-
-    if (currentHP > 0) {
-      sr.color = Color.white;
-      transform.localScale = originalScale;
-    }
-  }
-
-  void OnTriggerEnter2D(Collider2D collision) {
-    if (collision.gameObject.name == "AttackPoint") {
-      TakeDamage(25);
-    }
-  }
-
-  void TakeDamage(int damage) {
-    currentHP -= damage;
-
-    // knockback
-    if (target.position.x < transform.position.x) {
-      rb.velocity = new Vector2(5 * speed, rb.velocity.y);
-    } else {
-      rb.velocity = new Vector2(-5 * speed, rb.velocity.y);
-    }
-
-    // flash red and squish
-    StartCoroutine(DamageAnimation());
-  }
-
-  void FlipSlime() {
-    isFacingRight = !isFacingRight;
-    Vector3 scaler = transform.localScale;
-    scaler.x *= -1;
-    transform.localScale = scaler;
-  }
+//     // flash red and squish
+//     StartCoroutine(DamageAnimation());
+//   }
 }
