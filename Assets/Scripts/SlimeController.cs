371d28a415602fa038dfe9329d6bee3c63f03ff9
rivera26 <rivera26@pdx.edu>
2022-02-12 16:10:08 -0800

    more formatting standardization

--- Assets/Scripts/SlimeController.cs
+++ Assets/Scripts/SlimeController.cs
@@ -1,5 +1,5 @@
 public class SlimeController : EnemyController {
-  public SlimeController() : base() {
-      attackRange = 0.5f;
-  } 
+    public SlimeController() : base() {
+        attackRange = 0.5f;
+    }
 }
